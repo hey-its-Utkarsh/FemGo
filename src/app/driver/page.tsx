416fab7cb283ad5cd1bd40c83c31192a7958c62d
Kumar Utkarsh <kummarutkkarsh@gmail.com>
2025-11-17 23:30:57 +0000

    error

--- src/app/driver/page.tsx
+++ src/app/driver/page.tsx
@@ -78,6 +78,7 @@ export default function DriverDashboard() {
             }
         }
         return () => {
+            stopNotificationSound();
             if (requestIntervalRef.current) {
                 clearInterval(requestIntervalRef.current);
             }
